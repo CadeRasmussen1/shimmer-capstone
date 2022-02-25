189e50f9d7b92f96af5eabbc51fe7ea66265d312
Cade Rasmussen <caderasmussen@gmail.com>
2022-02-25 11:49:42 -0700

    attempted import error heroku

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import {Routes, Route} from 'react-router-dom';
-import Landing from './components/landing/landing.js';
-import SignIn from './components/signin/Signin.js';
-import Register from './components/register/register.js';
+import { Landing } from './components/landing/landing.js';
+import { Signin } from './components/signin/Signin.js';
+import { Register } from './components/register/register.js';
 
 
 export default(
     <Routes>
         <Route exact path="/" component={Landing}/>
-        <Route path="/signIn" component={SignIn}/>
+        <Route path="/signin" component={Signin}/>
         <Route path="/register" component={Register}/>
     </Routes>
 );
